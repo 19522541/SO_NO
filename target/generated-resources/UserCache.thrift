9ab755d8ea91b03b53c8eaf5d93d24d79e5f4534
19522541 <19522541@gm.uit.edu.vn>
2022-08-24 10:18:10 +0700

    remove: target folder

--- target/generated-resources/UserCache.thrift
+++
@@ -1,8 +0,0 @@
-#@namespace scala com.projectz.service
-include "UserCacheDT.thrift"
-
-service TUserCacheService {
-    string ping()
-    void addUser(1: required UserCacheDT.TUserInfo userInfo)
-    UserCacheDT.TUserInfo getUser(1:required UserCacheDT.TUserID userId)
-}
