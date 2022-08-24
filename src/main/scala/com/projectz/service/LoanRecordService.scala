1d8cfd6ba43a016fd9d162f1e929a9058148800a
19522541 <19522541@gm.uit.edu.vn>
2022-08-24 16:19:07 +0700

    define LoanRepository

--- src/main/scala/com/projectz/service/LoanRecordService.scala
+++ src/main/scala/com/projectz/service/LoanRecordService.scala
@@ -2,6 +2,7 @@ package com.projectz.service
 
 import com.projectz.domain.LoanRecord
 import com.projectz.domain.response.LoanResponse
+import com.projectz.repository.LoanRecordRepository
 import com.twitter.finatra.http.exceptions.NotFoundException
 
 import javax.inject.Inject
@@ -21,8 +22,7 @@ trait LoanService {
 
   /**
    * Chủ nợ lấy ra danh sách con nợ của mình
-   *
-   * @param lenderId dataType String
+   * @param lenderId Id của chủ nợ
    * @param keyword  Từ khóa để tìm kiếm con nợ.Trong chường hợp keyword trống thì sẽ hiện ra tất cả con nợ của chủ nợ
    * @return Danh sách các LoanResponse Object về tổng nợ của con nợ , với ngày tạo là của lần tạo ghi nợ gần nhất
    */
@@ -32,7 +32,6 @@ trait LoanService {
 
   /**
    * Chủ nợ lấy danh sách ghi nợ mà con nợ đã nợ mình
-   *
    * @param lenderId  Id của chủ nợ
    * @param borrowerId  Id của con nợ
    * @return Trả về danh sách các ghi nợ đã thêm
@@ -40,37 +39,27 @@ trait LoanService {
   @throws[NotFoundException]("Khi chủ nợ không tồn tại")
   @throws[InternalError]("gap bat cu exception nao")
   def getLoanDetailOfBorrower(lenderId: String, borrowerId: String): Seq[LoanResponse]
-
 }
   //Chưa thực hiện đc gì cả
   class LoanRecordServiceImpl @Inject()(
-                                         //    LoanRecordRepository: LoanRecordRepository[String, LoanRecord]
+                                             loanRecordRepository: LoanRecordRepository
                                        ) extends LoanService {
-    /**
-     * Chủ nợ thêm ghi nợ mới
-     *
-     * @param loanRecord Chi tiết nợ
-     * @return Trả về thông tin khi nợ mà chủ nợ  mới thực hiện thêm vào
-     */
-    override def addRecord(loanRecord: LoanRecord): LoanRecord = ???
 
-    /**
-     * Chủ nợ lấy ra danh sách con nợ của mình
-     *
-     * @param lenderId dataType String
-     * @param keyword  Từ khóa để tìm kiếm con nợ.Trong chường hợp keyword trống thì sẽ hiện ra tất cả con nợ của chủ nợ
-     * @return Danh sách các LoanResponse Object về tổng nợ của con nợ , với ngày tạo là của lần tạo ghi nợ gần nhất
-     */
-    override def getBorrowersOfLender(lenderId: String, keyword: String): Seq[LoanResponse] = ???
+    override def addRecord(loanRecord: LoanRecord): LoanRecord = {
+      val newRecord: LoanRecord = loanRecordRepository.save(loanRecord)
+      return newRecord
+    }
 
-    /**
-     * Chủ nợ lấy danh sách ghi nợ mà con nợ đã nợ mình
-     *
-     * @param lenderId   Id của chủ nợ
-     * @param borrowerId Id của con nợ
-     * @return Trả về danh sách các ghi nợ đã thêm
-     */
-    override def getLoanDetailOfBorrower(lenderId: String, borrowerId: String): Seq[LoanResponse] = ???
+
+    override def getBorrowersOfLender(lenderId: String, keyword: String): Seq[LoanResponse] = {
+      val borrowers :Seq[LoanResponse]= loanRecordRepository.getBorrower(lenderId,keyword)
+      borrowers
+    }
+
+
+    override def getLoanDetailOfBorrower(lenderId: String, borrowerId: String): Seq[LoanResponse] = {
+      val loadDetailOfBorrower :Seq[LoanResponse]= loanRecordRepository.getRecordOfBorrower(lenderId,borrowerId)
+    }
   }
 
 
