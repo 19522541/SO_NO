2e3aedbba573893c0b5b4ed903621b2795ae30ce
19522541 <19522541@gm.uit.edu.vn>
2022-08-24 10:32:08 +0700

    loan response

--- src/main/scala/com/projectz/controller/http/LoanRecordController.scala
+++ src/main/scala/com/projectz/controller/http/LoanRecordController.scala
@@ -2,7 +2,7 @@ package com.projectz.controller.http
 
 import com.projectz.domain.request.{AddLoanRecordRequest, GetLoanRecordRequest, UpdateLoanRecordRequest}
 import com.projectz.domain.{LoanRecord, UserID}
-import com.projectz.domain.response.{AddLoanResponse, DeletedLoanResponse, GetLoanResponse, UpdateLoanResponse}
+import com.projectz.domain.response.{AddLoanResponse, DeletedLoanResponse, GetLoanResponse, LoanResponse, UpdateLoanResponse}
 import com.projectz.service.LoanService
 import com.twitter.finagle.http.Request
 import com.twitter.finatra.http.Controller
@@ -13,28 +13,23 @@ import javax.inject.Inject
 class LoanRecordController @Inject()(loanRecordService: LoanService) extends Controller {
   get("/loan/:id") { request: GetLoanRecordRequest => {
     //val userLoanRecord:Seq[LoanRecord]  =loanRecordService.getRecord(request)
-    GetLoanResponse(UserID = "USERCODE1",
-      gottenLoanRecords = Seq(LoanRecord(id = "001",
+    LoanResponse(id = "002",
+      LoanRecord(id = "002",
         loanAmount = 10000,
         deadline = "Sun 2022.08.18",
-        borrower = "USERCODE2",
+        borrower = "USERCODE5",
         borrowDate = "Sun 2022.07.18",
-        lender = "USERCODE1"),
-        LoanRecord(id = "002",
-          loanAmount = 10000,
-          deadline = "Sun 2022.08.18",
-          borrower = "USERCODE5",
-          borrowDate = "Sun 2022.07.18",
-          lender = "USERCODE1"))
+        lender = "USERCODE1")
     )
 
   }
   }
   put("/loan:id") { request: UpdateLoanRecordRequest => {
-    val updatedRecordItem: LoanRecord = loanRecordService.updateRecord(request)
-    UpdateLoanResponse(
-      LoanID = "001",
-      LoanRecord(id = "002",
+    //val updatedRecordItem: LoanRecord = loanRecordService.updateRecord(request)
+
+    LoanResponse(
+      id = "001",
+      LoanRecord(id = "001",
         loanAmount = 10000,
         deadline = "Sun 2022.08.18",
         borrower = "USERCODE5",
@@ -46,8 +41,8 @@ class LoanRecordController @Inject()(loanRecordService: LoanService) extends Con
   post("/loan/:id") {
     request: AddLoanRecordRequest => {
       //val addedItemRecordItem :LoanRecord=  loanRecordService.addRecord(request.loanRecord)
-      AddLoanResponse(userID = "USERCODE1",
-        addedLoan = LoanRecord(id = "003",
+      LoanResponse(id = "003",
+        LoanRecord(id = "003",
           loanAmount = 10000,
           deadline = "Sun 2022.08.18",
           borrower = "USERCODE2",
@@ -59,8 +54,8 @@ class LoanRecordController @Inject()(loanRecordService: LoanService) extends Con
   delete("/loan/:id") {
     request: Request => {
       //val deletedRecordItem: LoanRecord=   loanRecordService.deleteRecord(request.getParam("id"))
-      DeletedLoanResponse(deletedLoanID = "003",
-        deletedLoan = LoanRecord(id = "003",
+      LoanResponse(id = "003",
+        LoanRecord(id = "003",
           loanAmount = 10000,
           deadline = "Sun 2022.08.18",
           borrower = "USERCODE2",
